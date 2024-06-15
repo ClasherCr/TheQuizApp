3b3d157ea2e27cae988029088b1c0aaba9be134d
Jashkumar Solanki <jashkumarsolanki@gmail.com>
2024-06-15 17:01:40 +0530

    Running The Activity

--- app/src/main/java/com/example/thequizapp/MainActivity.java
+++ app/src/main/java/com/example/thequizapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.thequizapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +8,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.thequizapp.databinding.ActivityMainBinding;
+import com.example.thequizapp.model.Question;
+import com.example.thequizapp.model.QuestionList;
+import com.example.thequizapp.viewmodel.QuizViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    QuizViewModel quizViewModel;
+    List<Question> questionsList;
+
+    static int result = 0;
+    static int totalQuestions = 0;
+    int i = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this,
+                R.layout.activity_main
+        );
+
+        result = 0;
+        totalQuestions = 0;
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        quizViewModel = new ViewModelProvider(this)
+                .get(QuizViewModel.class);
+        DisplayFirstQuestion();
+
     }
+
+    public void DisplayFirstQuestion() {
+        quizViewModel.getQuestionListLiveData().observe(this, new Observer<QuestionList>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onChanged(QuestionList questions) {
+                questionsList = questions;
+
+                binding.txtQuestion.setText("Question 1: " + questionsList.get(0).getQuestion());
+                binding.radio1.setText(questions.get(0).getOption1());
+                binding.radio2.setText(questions.get(0).getOption2());
+                binding.radio3.setText(questions.get(0).getOption3());
+                binding.radio4.setText(questions.get(0).getOption4());
+            }
+        });
+        }
 }
+
